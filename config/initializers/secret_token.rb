c8f3ac9a0064d0de845a52d30c46c3e49ec0d604
Erin Morrissey <erinmorrissey@gmail.com>
2016-08-12 22:33:33 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foood::Application.config.secret_key_base = '5c419152b8775af525d13f7ea4b8d04414246cb9e9a13616003ea07c9c3dc1eca38dd6d3bfcdc08d5593752107494abb577edb6556b4a482f599ee84a25223ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foood::Application.config.session_store :cookie_store, key: '_foood_session'
